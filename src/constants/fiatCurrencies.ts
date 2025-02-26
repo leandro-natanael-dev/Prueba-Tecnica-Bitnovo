374a2867bc380fb6618cdaa57f9e6fd8eca09e16
leandro-natanael-dev <leandro.almiron.dev@gmail.com>
2025-02-26 10:13:27 -0300

    Add fiatCurrencies file with currency data

---
+++ src/constants/fiatCurrencies.ts
@@ -0,0 +1,20 @@
+const fiatCurrencies = [
+  {
+    symbol: 'EUR',
+    name: 'Euro',
+    image: require('../assets/images/EUR.png'),
+  },
+  {
+    symbol: 'USD',
+    name: 'Dolar Estadounidense',
+
+    image: require('../assets/images/USD.png'),
+  },
+  {
+    symbol: 'GBP',
+    name: 'Libra Esterlina',
+    image: require('../assets/images/GBP.png'),
+  },
+];
+
+export default fiatCurrencies;
